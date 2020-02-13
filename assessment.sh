31c61fea2860a284309d51b7a78d3c4515503e2f
Callum.Wyres <callum.wyres@gmail.com>
2020-02-13 07:01:53 +0000

    added app insights

--- assessment.sh
+++ assessment.sh
@@ -31,7 +31,7 @@ terraform init
 
 echo "Creating Azure resource group and Azure Container Registry...."
 terraform apply -target azurerm_resource_group.devopsassessment -target azurerm_container_registry.acr --auto-approve
-################ Build docker image 
+################ Build docker image
 cd ..
 echo "Building docker image and pushing to acr...."
 
@@ -41,5 +41,6 @@ az acr build --registry devopsassessment --image devops-assessment .
 cd terraform
 terraform apply -target azurerm_app_service_plan.assessment -target azurerm_app_service.assessment --auto-approve
 terraform apply -target azurerm_monitor_autoscale_setting.assessment --auto-approve
+terraform apply -target azurerm_application_insights.appinsight --auto-approve
 
 curl https://cwdevops-appservice.azurewebsites.net/healthcheck
